d48bed59d96eb51bf034f8bd89532ce80f525122
Jason Lin <linjason920523@gmail.com>
2020-07-23 10:59:48 +0800

    create Dialog UI system

--- public/src/dialog.js
+++ public/src/dialog.js
@@ -1,13 +1,48 @@
-function showDialog(data){
-  $(".dialog>.title").text((data && "title" in data)?data.title: "Dialog");
-
-  $(".dialog-container").fadeIn(150);
-}
-
 (()=>{
   window.addEventListener("load",()=>{
     $(".dialog>.close").click(()=>{
       $(".dialog-container").fadeOut(150);
     })
-  })
+  });
+
+  function showDialog(data){
+    $(".dialog>.title").text((data && "title" in data)?data.title: "Dialog");
+    if(data && "layout" in data){
+      var duiContainer  = getDialogUI(data.layout);
+      $(".dialog>.dui").html(duiContainer.html);
+    }
+    
+    $(".dialog-container").fadeIn(150);
+  }
+
+  var duiBuilder = {
+    column: (data)=>{
+      return data.child.reduce((acc, cur)=>{
+        return acc + `<div class="dui-textbody">${cur}</div>`
+      },"");
+    }
+  }
+
+  function getDialogUI(layout){
+    return {
+      html:duiBuilder[layout.type](layout)
+    };
+  }
+
+  window.showDialog = showDialog;
+  window.dui = {
+    Column: (data)=>{
+      return {
+        type:"column",
+        child: data.child
+      };
+    },
+    Text: (data)=>{
+      return {
+        type:"text",
+        text: data.text,
+        other: data
+      };
+    }
+  }
 })();
