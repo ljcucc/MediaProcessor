0ee40473c9b3ab4dc153d04c447bca417f1a7ad7
ljcucc <ljcucc@gmail.com>
2020-07-26 10:51:53 +0800

    create canvas API

--- public/src/canvas.js
+++ public/src/canvas.js
@@ -1,9 +1,28 @@
 (()=>{
   var uuid, events;
 
+  var drawFunction = {
+    Color: (r,g,b)=>{
+      if(r && g && b){
+        return [r,g,b]
+      }
+      return [r,r,r];
+    },
+    setFillColor: (ctx, color)=>{
+      console.log(`rgb(${color[0]}, ${color[1], color[2]})`)
+      ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    },
+    Rect:(ctx, x,y, width, height)=>{ 
+      ctx.fillRect(x, y, width, height);
+    },
+    Image:(ctx)=>{
+
+    }
+  };
+
   function getHTMLTemplate(data){
     return `<canvas id="${data.uuid}" width="${data?.width || 320} height="${data?.height || 240}">
-        <p>Opsss... Your browser doesn't support canvas.</p>
+      <p>Opsss... Your browser doesn't support canvas.</p>
     </canvas>`;
   }
 
@@ -11,12 +30,37 @@
     events = e;
     uuid = id;
 
+    let canvas = $("#"+id).get(0);
+
+    var mouse;
+
+    function getMousePos(canvas, evt) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+      };
+    }
+
+    canvas.addEventListener('mousemove', function(evt) {
+      mouse = getMousePos(canvas, evt);
+
+      if(events?.onMouseMove)
+      events.onMouseMove(mouse);
+    }, false);
+
     if(events?.onStart)
-      events.onStart($("#"+id).getContext('2d'));
+      events.onStart(canvas.getContext('2d'), canvas);
+    
+    if(events?.onMouseMove)
+      $("#"+id).mousemove(()=>{
+        onMouseMove(mouse);
+      });
   }
 
   window.duiCanvas = {
     getHTMLTemplate,
-    registerEvent
+    registerEvent,
+    draw:drawFunction
   };
 })();
