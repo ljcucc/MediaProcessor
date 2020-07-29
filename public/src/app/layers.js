0d9a389572100c295e55d5c9172e36f9431c6991
ljcucc <linjason920523@gmail.com>
2020-07-29 20:47:55 +0800

    fix error

--- public/src/app/layers.js
+++ public/src/app/layers.js
@@ -1,17 +1,18 @@
 (()=>{
   let id = "layers_list"
-  window.dui.createDUI({
-    id,
-    name: "LayersList",
-    constructor: ()=>{
-      let uuid = uuidv4();
+  // window.dui.createDUI({
+  //   id,
+  //   name: "LayersList",
+  //   constructor: ()=>{
+  //     let uuid = uuidv4();
 
-      
-
-      return {
-        uuid,
-        type: id
-      };
-    }
-  });
+  //     return {
+  //       uuid,
+  //       type: id
+  //     };
+  //   },
+  //   template:()=>{
+  //     return "";
+  //   }
+  // });
 })();
