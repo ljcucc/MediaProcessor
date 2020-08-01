c207459ee8e470513e028e5fb72ff122b07c3b9b
ljcucc <linjason920523@gamil.com>
2020-08-02 01:31:11 +0800

    update unit test

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,7 @@
 (()=>{
   let renderBtn = document.querySelector("#render");
+  let txtarea = document.querySelector("#layers");
+  
   renderBtn.addEventListener("click", e=>{
     let layersCode = document.querySelector("#layers");
     console.log(layers.parse({
@@ -8,4 +10,9 @@
       level:0
     }));
   });
+  
+  document.addEventListener("keypress", e=>{
+    e.preventDefault();
+    console.log(e);
+  })
 })();
