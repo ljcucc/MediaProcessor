845d714590a978d48d2589634bb1bbe1f33c3c06
ljcucc <linjason920523@gmail.com>
2020-08-08 14:45:34 +0800

    add markdown support to helper

--- public/src/helper.js
+++ public/src/helper.js
@@ -3,29 +3,45 @@
   let cur_id = "";
 
   const database = {
-    "::welcome":`
-    <p>
-      Welcome to use MeidaProcessor. find more help and resources on <a href="https://ljcucc.github.io/docs/media-processor/index.html">https://ljcucc.github.io/docs/media-processor/index.html</a>
-    </p>
+    "::welcome":`# Welcome
+      Welcome to use **MeidaProcessor**.find more help and resources on https://ljcucc.github.io/docs/media-processor/index.html
 
-    <p>
       Press [Tab] key while you typing to get more help.
-    </p>
+
     `,
     "background":`
-    <p>
+
+    Porperty:
     background: [style]
-    </p>
+
     background is an property that setting the background style of the canvas.
     `
   };
+
+  let getHelperDocs = (id)=>{
+    let URLRegex = /(http|https):\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    let titleRegex = /^(#{1,6}\s+[\S]+)/m;
+
+    let markdown = database[id];
+
+    console.log(markdown.match(titleRegex))
+
+    markdown = (markdown.match(URLRegex) || []).reduce((acc, cur)=>acc.replace(cur, `<a target="_blank" href="${cur}">${cur}</a>`), markdown);
+    markdown = (markdown.match(titleRegex) || []).reduce((acc, cur)=>acc.replace(cur, `<h${cur.trim().lastIndexOf("#")+1} class="title helper">${cur.replace("#","")}</h${cur.trim().lastIndexOf("#")+1}>`), markdown)
+    markdown = (markdown.match(/\*\*(.*?)\*\*/gm) || []).reduce((acc, cur)=>acc.replace(cur, `<b>${cur.substring(2, cur.length-2)}</b>`), markdown)
+
+    markdown = markdown.replace(/\n/g, "<br>")+"\n";
+    console.log(markdown);
+
+    return markdown;
+  }
 
   let update = (id)=>{
     id = id.trim()
     if(cur_id == id) return;
 
     if(id in database)
-      helperDOM.innerHTML = database[id];
+      helperDOM.innerHTML = getHelperDocs(id);
 
     cur_id = id;
   }
