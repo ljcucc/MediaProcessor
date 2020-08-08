3cdf5f06e4e2685930d02f619843983fe43f5bc8
ljcucc <linjason920523@gmail.com>
2020-08-08 12:39:54 +0800

    add new helper info

--- public/src/helper.js
+++ public/src/helper.js
@@ -1,8 +1,9 @@
 (()=>{
   let helperDOM;
+  let cur_id = "";
 
   const database = {
-    "::weclome":`
+    "::welcome":`
     <p>
       Welcome to use MeidaProcessor. find more help and resources on <a href="https://ljcucc.github.io/docs/media-processor/index.html">https://ljcucc.github.io/docs/media-processor/index.html</a>
     </p>
@@ -10,18 +11,41 @@
     <p>
       Press [Tab] key while you typing to get more help.
     </p>
+    `,
+    "background":`
+    <p>
+    background: [style]
+    </p>
+    background is an property that setting the background style of the canvas.
     `
   };
 
-  let activate = (linetext)=>{
-    console.log("Enter helper.activate");
+  let update = (id)=>{
+    id = id.trim()
+    if(cur_id == id) return;
+
+    if(id in database)
+      helperDOM.innerHTML = database[id];
+
+    cur_id = id;
   }
 
-  window.addEventListener("load", e=>{
+  let activate = (linetext)=>{
+    linetext = String(linetext);
+
+    console.log("Enter helper.activate");
+    console.log(linetext);
+
+    if(linetext.trim().indexOf("+") == 0){
+      update(linetext.trim().replace("+","").split(":")[1]);
+    }else if(linetext.trim().indexOf("-") == 0){
+      update(linetext.trim().replace("-","").split(":")[0]);
+    }
+  }
+
+  window.addEventListener("load",async  e=>{
     helperDOM = document.querySelector("#helper");
-    console.log( database["::weclome"])
-    console.log(helperDOM)
-    helperDOM.innerHTML = database["::weclome"];
+    await update("::welcome")
   })
 
   window.helper = {
