529b35ad3d5b26c9f2c33792b6e23edb064eb9aa
Jason Lin <linjason920523@gmail.com>
2020-10-27 01:25:04 +0800

    fix bug

--- src/main.c
+++ src/main.c
@@ -1,5 +1,31 @@
 #include<stdio.h>
+#include<string.h>
+
+void writeIndex();
+
+struct Page{
+  char filename[20];
+};
+
+void makepage(struct Page *page, char *filename);
 
 int main(){
+  struct Page home;
+  makepage(&home, "index.html");
+
+  writeIndex();
   return 0;
+}
+
+void makepage(struct Page *page, char *filename){
+ *page->filename = *filename;
+
+  printf("processing %s ...", filename);
+}
+
+
+void writeIndex(struct Page page){
+  FILE *indexFile = fopen(page.filename,"a");
+
+  fclose(indexFile);
 }
